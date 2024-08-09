c2fd5e19842a56a9c494a60f0002a19b28c62ae1
PARAG\parag <31978917+paragpathak2006@users.noreply.github.com>
2024-08-08 20:11:27 -0400

    commit

---
+++ Input_output/Loader.cpp
@@ -0,0 +1,284 @@
+
+#include "Loader.h"
+
+// Namespace: Math
+//
+// Description: The namespace that holds all of the math
+//	functions need for OBJL
+namespace objl
+{
+	namespace math
+	{
+		// Vector3 Cross Product
+		Vector3 CrossV3(const Vector3 a, const Vector3 b)
+		{
+			return Vector3(a.Y * b.Z - a.Z * b.Y,
+				a.Z * b.X - a.X * b.Z,
+				a.X * b.Y - a.Y * b.X);
+		}
+
+		// Vector3 Magnitude Calculation
+		float MagnitudeV3(const Vector3 in)
+		{
+			return (sqrtf(powf(in.X, 2) + powf(in.Y, 2) + powf(in.Z, 2)));
+		}
+
+		// Vector3 DotProduct
+		float DotV3(const Vector3 a, const Vector3 b)
+		{
+			return (a.X * b.X) + (a.Y * b.Y) + (a.Z * b.Z);
+		}
+
+		// Angle between 2 Vector3 Objects
+		float AngleBetweenV3(const Vector3 a, const Vector3 b)
+		{
+			float angle = DotV3(a, b);
+			angle /= (MagnitudeV3(a) * MagnitudeV3(b));
+			return angle = acosf(angle);
+		}
+
+		// Projection Calculation of a onto b
+		Vector3 ProjV3(const Vector3 a, const Vector3 b)
+		{
+			Vector3 bn = b / MagnitudeV3(b);
+			return bn * DotV3(a, bn);
+		}
+	}
+
+	// Namespace: Algorithm
+	//
+	// Description: The namespace that holds all of the
+	// Algorithms needed for OBJL
+	namespace algorithm
+	{
+		// Vector3 Multiplication Opertor Overload
+		Vector3 operator*(const float& left, const Vector3& right)
+		{
+			return Vector3(right.X * left, right.Y * left, right.Z * left);
+		}
+
+		// A test to see if P1 is on the same side as P2 of a line segment ab
+		bool SameSide(Vector3 p1, Vector3 p2, Vector3 a, Vector3 b)
+		{
+			Vector3 cp1 = math::CrossV3(b - a, p1 - a);
+			Vector3 cp2 = math::CrossV3(b - a, p2 - a);
+
+			if (math::DotV3(cp1, cp2) >= 0)
+				return true;
+			else
+				return false;
+		}
+
+		// Generate a cross produect normal for a triangle
+		Vector3 GenTriNormal(Vector3 t1, Vector3 t2, Vector3 t3)
+		{
+			Vector3 u = t2 - t1;
+			Vector3 v = t3 - t1;
+
+			Vector3 normal = math::CrossV3(u, v);
+
+			return normal;
+		}
+
+		// Check to see if a Vector3 Point is within a 3 Vector3 Triangle
+		bool inTriangle(Vector3 point, Vector3 tri1, Vector3 tri2, Vector3 tri3)
+		{
+			// Test to see if it is within an infinite prism that the triangle outlines.
+			bool within_tri_prisim = SameSide(point, tri1, tri2, tri3) && SameSide(point, tri2, tri1, tri3)
+				&& SameSide(point, tri3, tri1, tri2);
+
+			// If it isn't it will never be on the triangle
+			if (!within_tri_prisim)
+				return false;
+
+			// Calulate Triangle's Normal
+			Vector3 n = GenTriNormal(tri1, tri2, tri3);
+
+			// Project the point onto this normal
+			Vector3 proj = math::ProjV3(point, n);
+
+			// If the distance from the triangle to the point is 0
+			//	it lies on the triangle
+			if (math::MagnitudeV3(proj) == 0)
+				return true;
+			else
+				return false;
+		}
+
+		// Split a String into a string array at a given token
+		inline void split(const std::string& in,
+			std::vector<std::string>& out,
+			std::string token)
+		{
+			out.clear();
+
+			std::string temp;
+
+			for (int i = 0; i < int(in.size()); i++)
+			{
+				std::string test = in.substr(i, token.size());
+
+				if (test == token)
+				{
+					if (!temp.empty())
+					{
+						out.push_back(temp);
+						temp.clear();
+						i += (int)token.size() - 1;
+					}
+					else
+					{
+						out.push_back("");
+					}
+				}
+				else if (i + token.size() >= in.size())
+				{
+					temp += in.substr(i, token.size());
+					out.push_back(temp);
+					break;
+				}
+				else
+				{
+					temp += in[i];
+				}
+			}
+		}
+
+		// Get tail of string after first token and possibly following spaces
+		inline std::string tail(const std::string& in)
+		{
+			size_t token_start = in.find_first_not_of(" \t");
+			size_t space_start = in.find_first_of(" \t", token_start);
+			size_t tail_start = in.find_first_not_of(" \t", space_start);
+			size_t tail_end = in.find_last_not_of(" \t");
+			if (tail_start != std::string::npos && tail_end != std::string::npos)
+			{
+				return in.substr(tail_start, tail_end - tail_start + 1);
+			}
+			else if (tail_start != std::string::npos)
+			{
+				return in.substr(tail_start);
+			}
+			return "";
+		}
+
+		// Get first token of string
+		inline std::string firstToken(const std::string& in)
+		{
+			if (!in.empty())
+			{
+				size_t token_start = in.find_first_not_of(" \t");
+				size_t token_end = in.find_first_of(" \t", token_start);
+				if (token_start != std::string::npos && token_end != std::string::npos)
+				{
+					return in.substr(token_start, token_end - token_start);
+				}
+				else if (token_start != std::string::npos)
+				{
+					return in.substr(token_start);
+				}
+			}
+			return "";
+		}
+
+		// Get element at given index position
+		template <class T>
+		inline const T& getElement(const std::vector<T>& elements, std::string& index)
+		{
+			int idx = std::stoi(index);
+			if (idx < 0)
+				idx = int(elements.size()) + idx;
+			else
+				idx--;
+			return elements[idx];
+		}
+	}
+}
+
+void get_mesh(const std::string& file, objl::Mesh& mesh) {
+	objl::Loader Loader;
+	bool loadout = Loader.LoadFile(file);
+
+	if (!loadout)
+		mesh = objl::Mesh();
+	else
+		mesh = Loader.LoadedMeshes[0];
+}
+
+#define _MAX_OF_(A,B) ( A > B ) ? A : B 
+#define _MIN_OF_(A,B) ( A < B ) ? A : B 
+
+
+void test_load_file() {
+
+	// Initialize Loader
+	objl::Loader Loader;
+
+	// Load .obj File
+	bool loadout = Loader.LoadFile("cube.obj");
+
+	if (loadout)
+	{
+		// Create/Open e1Out.txt
+		std::ofstream file("e1Out.txt");
+
+		// Go through each loaded mesh and out its contents
+		for (int i = 0; i < Loader.LoadedMeshes.size(); i++)
+		{
+			// Copy one of the loaded meshes to be our current mesh
+			objl::Mesh curMesh = Loader.LoadedMeshes[i];
+
+			// Print Mesh Name
+			file << "Mesh " << i << ": " << curMesh.MeshName << "\n";
+
+			// Print Vertices
+			file << "Vertices:\n";
+
+			// Go through each vertex and print its number,
+			//  position, normal, and texture coordinate
+			for (int j = 0; j < curMesh.Vertices.size(); j++)
+			{
+				file << "V" << j << ": " <<
+					"P(" << curMesh.Vertices[j].Position.X << ", " << curMesh.Vertices[j].Position.Y << ", " << curMesh.Vertices[j].Position.Z << ") " <<
+					"N(" << curMesh.Vertices[j].Normal.X << ", " << curMesh.Vertices[j].Normal.Y << ", " << curMesh.Vertices[j].Normal.Z << ") " <<
+					"TC(" << curMesh.Vertices[j].TextureCoordinate.X << ", " << curMesh.Vertices[j].TextureCoordinate.Y << ")\n";
+			}
+
+			// Print Indices
+			file << "Indices:\n";
+
+			// Go through every 3rd index and print the
+			//	triangle that these indices represent
+			for (int j = 0; j < curMesh.Indices.size(); j += 3)
+				file << "T" << j / 3 << ": " << curMesh.Indices[j] << ", " << curMesh.Indices[j + 1] << ", " << curMesh.Indices[j + 2] << "\n";
+
+			// Print Material
+			file << "Material: " << curMesh.MeshMaterial.name << "\n";
+			file << "Ambient Color: " << curMesh.MeshMaterial.Ka.X << ", " << curMesh.MeshMaterial.Ka.Y << ", " << curMesh.MeshMaterial.Ka.Z << "\n";
+			file << "Diffuse Color: " << curMesh.MeshMaterial.Kd.X << ", " << curMesh.MeshMaterial.Kd.Y << ", " << curMesh.MeshMaterial.Kd.Z << "\n";
+			file << "Specular Color: " << curMesh.MeshMaterial.Ks.X << ", " << curMesh.MeshMaterial.Ks.Y << ", " << curMesh.MeshMaterial.Ks.Z << "\n";
+			file << "Specular Exponent: " << curMesh.MeshMaterial.Ns << "\n";
+			file << "Optical Density: " << curMesh.MeshMaterial.Ni << "\n";
+			file << "Dissolve: " << curMesh.MeshMaterial.d << "\n";
+			file << "Illumination: " << curMesh.MeshMaterial.illum << "\n";
+			file << "Ambient Texture Map: " << curMesh.MeshMaterial.map_Ka << "\n";
+			file << "Diffuse Texture Map: " << curMesh.MeshMaterial.map_Kd << "\n";
+			file << "Specular Texture Map: " << curMesh.MeshMaterial.map_Ks << "\n";
+			file << "Alpha Texture Map: " << curMesh.MeshMaterial.map_d << "\n";
+			file << "Bump Map: " << curMesh.MeshMaterial.map_bump << "\n";
+
+			// Leave a space to separate from the next mesh
+			file << "\n";
+		}
+
+		file.close();
+	}
+	// If not output an error
+	else
+	{
+		std::ofstream file("e1Out.txt");
+		file << "Failed to Load File. May have failed to find it or it was not an .obj file.\n";
+		file.close();
+	}
+}
+
---
+++ divergent_test.cpp
@@ -0,0 +1,246 @@
+// divergent_test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<vector>
+#include<unordered_set>
+#include<unordered_map>
+#include "Input_output/Loader.h"
+union Point {
+    double x, y, z;
+};
+
+class Vertex {
+public:
+    Point point;
+    std::vector<int> adjVertices;
+    std::vector<int> adjFaces;
+    bool is_deleted = false;
+    Vertex(double X, double Y, double Z) {
+        point.x = X;
+        point.y = Y;
+        point.z = Z;
+        is_deleted = false;
+    }
+    void remove(bool flag = true, Faces& faces) {
+        is_deleted = true;
+        if (flag)
+            for (auto f : adjFaces)
+                faces[f].remove();
+    }
+
+};
+
+typedef std::vector<Vertex> Vertices;
+
+inline double dist(Point P, Point Q) {
+    return (P.x - Q.x) * (P.x - Q.x) + (P.y - Q.y) * (P.y - Q.y) + (P.z - Q.z) * (P.z - Q.z);
+}
+
+inline double dist(Vertex P, Vertex Q) {
+    return dist(P.point, Q.point);
+}
+
+class Face {
+public:
+    int vertices[3];
+    std::vector<int> adjFaces;
+    bool is_deleted = false;
+    Face(int v0, int v1, int v2) {
+        vertices[0] = v0;
+        vertices[1] = v1;
+        vertices[2] = v2;
+        is_deleted = false;
+    }
+    void flip_sense() {
+        std::swap(vertices[1], vertices[2]);
+    }
+    void remove() {
+        is_deleted = true;
+    }
+    bool check_below_threshold_angle(const Vertices& v, const double& cos2_angle) const {
+        const Vertex& i = v[vertices[0]];
+        const Vertex& j = v[vertices[1]];
+        const Vertex& k = v[vertices[2]];
+
+        const double a = dist(i, j);
+        const double b = dist(j, k);
+        const double c = dist(k, i);
+
+        if (c + b > a) { double cos2_a = (c + b - a) * (c + b - a) / (4 * c * b); if (cos2_angle > cos2_a) return true; }
+        if (a + c > b) { double cos2_b = (a + c - b) * (a + c - b) / (4 * a * c); if (cos2_angle > cos2_b) return true; }
+        if (a + b > c) { double cos2_c = (a + b - c) * (a + b - c) / (4 * a * b); if (cos2_angle > cos2_c) return true; }
+
+        return false;
+
+    }
+};
+typedef std::vector<Face> Faces;
+
+class Mesh {
+public:
+    Vertices vertices;
+    Faces faces;
+};
+
+class Edge {
+public:
+    int i, j;
+    Edge(int ii, int jj) { i = ii; j = jj; }
+    bool operator==(const Edge& b) const { return i == b.i && j == b.j; }
+};
+
+// Edge Hash functor
+struct Hash_of_Edge {
+    size_t operator() (const Edge& P) const {
+        return P.i * 18397 + P.j * 20483;
+    }
+};
+
+typedef std::unordered_set<Edge, Hash_of_Edge> Edges;
+
+bool checkOriention(Edges& edges, int i, int j) {
+    Edge edge(i, j);
+
+    if (edges.count(edge) != 0)
+        return false;
+
+    edges.emplace(i,j);
+    return true;
+}
+
+
+bool checkOriention(const Faces& faces) {
+    std::unordered_set<Edge, Hash_of_Edge> edges;
+    edges.reserve(faces.size() * 3);
+    for (const auto& face : faces) {
+        if (!checkOriention(edges, face.vertices[0], face.vertices[1])) return false;
+        if (!checkOriention(edges, face.vertices[1], face.vertices[2])) return false;
+        if (!checkOriention(edges, face.vertices[2], face.vertices[0])) return false;
+    }
+}
+
+void addEdges(Edges& edges, int i, int j,int& num_edges) {
+    Edge edge(j, i);
+
+    if (edges.count(edge) != 0)
+        return;
+
+    edges.emplace(i, j);
+    num_edges++;
+}
+
+typedef std::unordered_multimap<int, Edge> VertexToEdge;
+
+int num_loops(const Faces& faces, int num_vertices, int num_faces) {
+    std::unordered_set<Edge, Hash_of_Edge> edges;
+    int num_edges = 0;
+    edges.reserve(faces.size() * 3);
+    for (const auto& face : faces) {
+        addEdges(edges, face.vertices[0], face.vertices[1], num_edges);
+        addEdges(edges, face.vertices[1], face.vertices[2], num_edges);
+        addEdges(edges, face.vertices[2], face.vertices[0], num_edges);
+    }
+
+    int euler_characteristic = num_vertices - num_edges + num_faces;
+
+
+    return 2 - euler_characteristic;
+    //VertexToEdge vertexToEdgemap;
+    //vertexToEdgemap.reserve(edges.size() * 2);
+    //for (const auto& edge : edges) {
+    //    vertexToEdgemap.emplace(edge.i, edge);
+    //    vertexToEdgemap.emplace(edge.j, edge);
+    //}
+
+    //auto i = edges.begin()->i;
+    //auto j = edges.begin()->j;
+}
+
+void Edge_Colapse(Faces faces, Vertices v, int i,int j){
+    for (auto f : v[j].adjFaces) 
+        for(int ii = 0; ii<3;ii++) 
+            if (faces[f].vertices[ii] == j) 
+                faces[f].vertices[ii] = i;
+    
+    for (const auto& adjV : v[j].adjVertices)
+        for (auto& adjV2 : v[adjV].adjVertices)
+            if (adjV2 == j) 
+                adjV2 = i;
+
+    // add adjFaces to i
+}
+
+
+void get_opposite_vertices(int i, int& j, int& k) {
+    if (i == 0) { j == 1; k == 2; return; }
+    if (i == 1) { j == 2; k == 0; return; }
+    if (i == 2) { j == 0; k == 1; return; }
+}
+
+
+void diagonal_edge_swap(Face& f1, Face& f2) {
+    bool found = false;
+    int j1, k1;
+    int j2, k2;
+    for (size_t i1 = 0; i1 < 3; i1++)
+    {
+        get_opposite_vertices(i1, j1, k1);
+        for (size_t i2 = 0; i2 < 3; i2++)
+        {
+            get_opposite_vertices(i2, j2, k2);
+            if (f1.vertices[j1] == f2.vertices[k2] && f1.vertices[k1] == f2.vertices[j2])
+            {
+                f1.vertices[k1] = f2.vertices[i2];
+                f2.vertices[k2] = f1.vertices[i1];
+                return;
+            }
+        }
+
+    }
+
+}
+
+
+void get_points(const objl::Mesh& curMesh, Vertices& points) {
+    int length = curMesh.Vertices.size();
+
+    points.reserve(length);
+    for (int j = 0; j < length; j++) {
+        points.emplace_back(
+            curMesh.Vertices[j].Position.X,
+            curMesh.Vertices[j].Position.Y,
+            curMesh.Vertices[j].Position.Z
+        );
+    }
+}
+
+void get_faces(const objl::Mesh& curMesh, Faces& faces) {
+    int length = curMesh.Vertices.size();
+    faces.reserve(length);
+    for (int j = 0; j < length; j += 3)
+        faces.emplace_back(curMesh.Indices[j], curMesh.Indices[j + 1], curMesh.Indices[j + 2]);
+}
+
+int main()
+{
+    objl::Mesh mesh;    get_mesh("cube.obj", mesh);
+
+    Vertices vertices;      get_points(mesh, vertices);
+    Faces faces;        get_faces(mesh, faces);
+
+
+    
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
